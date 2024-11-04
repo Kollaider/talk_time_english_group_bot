1eb3fcc246c6923874f6150660e6569e040b4dd0
kolaider <nikolai.strom@skz.by>
2024-11-05 00:44:33 +0300

    set schedule: ever work day at 10am for sending meme

--- bot/tkq.py
+++ bot/tkq.py
@@ -23,9 +23,8 @@ scheduler = TaskiqScheduler(
     sources=[LabelScheduleSource(broker)],
 )
 
-
-@broker.task(schedule=[{"cron": "0 10 * * 1-5", "args": [1]}])
 # @broker.task(schedule=[{"cron": "*/1 * * * *", "args": [1]}])  # every minute
+@broker.task(schedule=[{"cron": "0 10 * * 1-5", "args": [1]}])
 async def heavy_task(value: int, bot: Bot = TaskiqDepends()):
     meme_url = await get_meme_url()
     await bot.send_photo(chat_id=CHAT_ID, photo=meme_url)
